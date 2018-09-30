62564b52e6332ca21c0c2a08b43cf20764a919b1
Jonathan Boggon <jonathanboggon@Jonathans-iMac.local>
2018-09-30 11:00:25 +0100

    no stations working

--- app/features/commute-form/epics/commute-form-epic.js
+++ app/features/commute-form/epics/commute-form-epic.js
@@ -2,12 +2,19 @@ import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import { catchError } from 'rxjs/operators';
 
 import {
   COMMUTE_FORM_REQUEST,
   receiveCommuteFormData,
   failedCommuteFormRequest
 } from 'features/commute-form/actions/commute-form-actions';
+
+const constants = {
+  tflAppId: 'b4aa26bb',
+  tflAppKey: '938e3b49ed68cab0d4b0191d4aa914aa'
+};
+
 /**
  * fetchCommuteFormData
  * @param {Object} action$ - action observable
@@ -19,13 +26,21 @@ export default function fetchCommuteFormData (action$, store, { apiHelper }) {
   return action$.ofType(COMMUTE_FORM_REQUEST)
     .switchMap(action =>
       Observable.fromPromise(apiHelper.get(
-        `https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanMetroStation,NaptanRailStation&radius=${action.mileRadius}&lat=${action.lat}&lon=${action.lng}`
-      )),
+        `https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanMetroStation,NaptanRailStation&radius=${action.mileRadius}&lat=${action.lat}&lon=${action.lng}&app_id=${constants.tflAppId}&app_key=${constants.tflAppKey}`
+      ))
+        .catch(error => {
+          const errorMessage =
+            error.message === 'Network Error'
+              ? 'There must be too many stations in this area!'
+              : error.message;
+
+          return Observable.of(failedCommuteFormRequest(errorMessage));
+        }),
     (action, response) => ([response, action]))
     .switchMap(
       ([response, action]) => {
         if (!response.data.stopPoints.length) {
-          throw new Error('There are no stations in this area.');
+          return Observable.of(failedCommuteFormRequest('There are no stations in this area.'));
         }
 
         return Observable.forkJoin(
@@ -34,33 +49,30 @@ export default function fetchCommuteFormData (action$, store, { apiHelper }) {
             .map(stopPoint =>
               Observable.fromPromise(
                 apiHelper.get(
-                  `https://api.tfl.gov.uk/Journey/JourneyResults/${stopPoint.icsCode}/to/${action.workStationIcsId}&time=0900`
+                  `https://api.tfl.gov.uk/Journey/JourneyResults/${stopPoint.icsCode}/to/${action.workStationIcsId}&time=0900&app_id=${constants.tflAppId}&app_key=${constants.tflAppKey}`
                 )
               )
             )
         )
+          .map(responses => {
+            const responseData = [];
+
+            responses.forEach(journeyResponse => {
+              responseData.push(journeyResponse.data.journeys[0]);
+            });
+
+            return receiveCommuteFormData(responseData);
+          })
+          .pipe(
+            catchError(error => {
+              const errorMessage =
+                error.response && error.response.data.toLocationDisambiguation.matchStatus === 'empty'
+                  ? 'Please enter a valid station for your work station'
+                  : error.message;
+
+              return Observable.of(failedCommuteFormRequest(errorMessage));
+            })
+          );
       }
-    )
-    .map(responses => {
-      const responseData = [];
-
-      responses.map(response => {
-        responseData.push(response.data.journeys[0]);
-      });
-
-      return receiveCommuteFormData(responseData);
-    })
-    .catch(error => {
-      let errorMessage = error.message;
-
-      if (error.message === 'Network Error') {
-        errorMessage = 'There must be too many stations in this area!';
-      }
-
-      if (error.response && error.response.data.toLocationDisambiguation.matchStatus === 'empty') {
-        errorMessage = 'Please enter a valid station for your work station.';
-      }
-
-      return Observable.of(failedCommuteFormRequest(errorMessage));
-    });
+    );
 }
