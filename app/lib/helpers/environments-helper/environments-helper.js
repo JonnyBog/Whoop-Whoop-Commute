3661b46f48b210a50edeefd9e636e7f0f41532cd
Jonathan Boggon <jonathanboggon@Jonathans-iMac.local>
2018-10-27 09:50:04 +0100

    station picker epic tests

--- app/lib/helpers/environments-helper/environments-helper.js
+++
@@ -1,5 +0,0 @@
-export default {
-  IS_CLIENT: (typeof window !== 'undefined' && window.document),
-  IS_DEVELOPMENT_ENV: process.env.NODE_ENV !== 'production',
-  IS_PROD: process.env.DEPLOYMENT_GROUP_NAME === 'prod' || process.env.NODE_ENV === 'production'
-};
